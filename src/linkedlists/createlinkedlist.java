7d02c316705cca8967481fec8f0877fad8473ea2
Matthew Kern <mattcs321@gmail.com>
2024-12-20 23:32:03 -0500

    create linked list skeleton pg 104-105

---
+++ src/linkedlists/createlinkedlist.java
@@ -0,0 +1,17 @@
+package linkedlists;
+
+public class createlinkedlist {
+	
+	
+	 static  void createll() {
+		
+		   
+		   
+	}
+   
+	public static void main(String[] args) {
+		System.out.println("");
+	}
+	
+
+}
