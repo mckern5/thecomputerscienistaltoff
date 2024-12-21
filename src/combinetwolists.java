2cd2da1dc12425518e78bbc3e1638c7c25382cbf
Matthew Kern <mattcs321@gmail.com>
2024-12-20 19:15:59 -0500

    combine2lists

---
+++ src/combinetwolists.java
@@ -0,0 +1,28 @@
+import java.util.stream.*;
+
+public class combinetwolists {
+	
+	
+	static String[] combine(String[] x, int[] y) {
+
+		String[] xx= {"ff"};
+		
+		return xx;//IntStream
+			//.range(0, Math.min(x.length, y.length))
+			//.mapToObj(i-> x.get(i)+":"+y.get(i))
+			
+		
+		
+	}
+	
+	public static void main (String[] args) {
+		
+		String[] movie={"INTER","incept","the prest","insomnia"};
+		int[] rating= {1,10,10,8,6};
+		
+		
+		
+		System.out.print(combine(movie,rating)[0]);
+	}
+
+}
